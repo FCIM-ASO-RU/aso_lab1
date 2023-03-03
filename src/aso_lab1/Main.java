78a6ec525745f16e4761af9e02937fbc68c4eaf2
Qrroll <dainsureivu@gmail.com>
2023-03-03 17:44:43 +0200

    create/PrisacariSerghei

--- src/aso_lab1/Main.java
+++ src/aso_lab1/Main.java
@@ -1,13 +1,15 @@
 package aso_lab1;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
     public static void main(String[] args) {
         final Timer timer = new Timer();
-        
-        
+        TimerTask taskPrisacari = new PrisacariSerghei();
+        timer.schedule(taskPrisacari, 10000);
+
     }
     
 }
---
+++ src/aso_lab1/PrisacariSerghei.java
@@ -0,0 +1,13 @@
+package aso_lab1;
+
+import java.util.TimerTask;
+
+public class PrisacariSerghei extends TimerTask {
+
+    @Override
+    public void run() {
+
+        System.out.println("Hello world!");
+    }
+
+}
