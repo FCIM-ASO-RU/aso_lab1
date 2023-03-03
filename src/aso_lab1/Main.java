8df4a8aca7b0e7db23252348f7c6c78330356b0a
Vsevolod Subbotkin <vsevolod.98t@mail.ru>
2023-03-03 14:26:07 +0200

    initial commit

---
+++ src/aso_lab1/Main.java
@@ -0,0 +1,13 @@
+package aso_lab1;
+
+import java.util.Timer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final Timer timer = new Timer();
+        
+        
+    }
+    
+}
