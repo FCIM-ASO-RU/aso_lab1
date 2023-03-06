cf41162de4c0438025b86db90dfbf07f9dd59a22
DMelnikk <dmitry.melnik001@mail.ru>
2023-03-06 14:11:09 +0200

    Создал класс MelnicDmitri , файл и в нём класс , цикл с одним знаком

--- src/aso_lab1/Main.java
+++ src/aso_lab1/Main.java
@@ -1,13 +1,18 @@
 package aso_lab1;
 
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
     public static void main(String[] args) {
         final Timer timer = new Timer();
-        
+        TimerTask taskMelnic = new MelnicDmitri();
+        timer.schedule(taskMelnic, 10000);
         
     }
     
 }
+
+
+
