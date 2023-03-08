ff8e3b6003031d4423f6833e420b8875cb587da4
Valelaaa <kosneanuvaleriy@gmail.com>
2023-03-08 20:21:00 +0300

    - Create class CosneanuValeriuImpl.java that should extend TimerTask. - CosneanuValeriuImpl override run method and makes run audio file. - Also create Log.java file for logging (specially for team_R).

--- src/aso_lab1/Main.java
+++ src/aso_lab1/Main.java
@@ -1,13 +1,22 @@
 package aso_lab1;
 
+import aso_lab1.team_rocket.CosneanuValeriuImpl;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.sql.Timestamp.valueOf;
 
 public class Main {
+    private static final String AUDIO_FILE_PATH = ".//Resources/static/audio/INSTASAMKA.wav";
 
     public static void main(String[] args) {
         final Timer timer = new Timer();
-        
-        
+        final TimerTask task = new CosneanuValeriuImpl(AUDIO_FILE_PATH, timer);
+        timer.schedule(task, valueOf(LocalDateTime.of(LocalDate.now(), LocalTime.now())));
     }
-    
+
 }
---
+++ src/aso_lab1/team_rocket/CosneanuValeriuImpl.java
@@ -0,0 +1,72 @@
+package aso_lab1.team_rocket;
+
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static aso_lab1.team_rocket.logger.Log.info;
+import static aso_lab1.team_rocket.logger.Log.infoFormat;
+
+
+/**
+ * Custom Implementation of TimerTask for ASO lab 1
+ */
+public class CosneanuValeriuImpl extends TimerTask {
+
+    private final Timer timer;
+    private Clip audioClip;
+    private long duration = 0;
+
+    /**
+     * Constructor
+     *
+     * @param path  path of audio file
+     * @param timer timer what should be bound with TimerTask
+     */
+    public CosneanuValeriuImpl(final String path, Timer timer) {
+        info("CONSTRUCTOR IS RUNNING");
+
+        this.timer = timer;
+        final File audioFile = new File(path);
+
+        try {
+            final AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            final AudioFormat format = audioStream.getFormat();
+            final DataLine.Info info = new DataLine.Info(Clip.class, format);
+            audioClip = (Clip) AudioSystem.getLine(info);
+            audioClip.open(audioStream);
+            duration = audioClip.getMicrosecondLength() / 1_000_000;
+        } catch (Exception exception) {
+            infoFormat("[ CONSTRUCTOR THROW EXCEPTION: {%s}]", exception.toString());
+        }
+    }
+
+    /**
+     * @return duration of audio file
+     */
+    public long getDuration() {
+        return duration;
+    }
+
+    /**
+     * overriding TimerTask run method
+     * start playing audio file
+     */
+    @Override
+    public void run() {
+        try {
+            audioClip.start();
+            info("METHOD: {run} IS RUNNING");
+            Thread.sleep(audioClip.getMicrosecondLength() / 1000);
+            audioClip.stop();
+            audioClip.setFramePosition(0);
+
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            timer.cancel();
+            infoFormat("ERROR PLAYING AUDIO {%s}", e.toString());
+        }
+    }
+}
