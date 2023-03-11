d7f912c896f5e2f870c6ec2fe8d38b8346a52b8e
Mihail2504 <miha-25.01@mail.ru>
2023-03-11 12:27:34 +0200

    Add more comments

--- src/aso_lab1/Money.java
+++ src/aso_lab1/Money.java
@@ -12,35 +12,36 @@ public class Money extends TimerTask {
 
     public Money(Timer timer) {
         this.timer = timer;
-    }
+    } // конструктор который принимает объект таймера
 
     public void run() {
         int amount = (int)(Math.random() * 1000);   //Math.random() генерирует от 0 - 1 , умножаем на 1000 и получаем результат от 0 до 1000
-        balance += amount;
-        System.out.println("Dobavleno " + amount + " deneg. Balans: " + balance);
-        if (balance >= GOAL) {
-            playSound();
-            System.out.println("Na dereviah rastut denigi!");
-            cancel();
-            timer.cancel();
+        balance += amount; // добавляем к балансу деньги
+        System.out.println("Dobavleno " + amount + " deneg. Balans: " + balance); // вывод на экран
+        if (balance >= GOAL) { // если баланс достиг цели, то
+            playSound(); // сыграть музыку
+            System.out.println("Na dereviah rastut denigi!"); // финальный вывод
+            cancel(); //закрыть задание
+            timer.cancel(); // остановить таймер
         }
     }
 
-    private void playSound() {
+    private void playSound() { // метод играть музыку
         try {
+            // открываем файл музыки и стартуем аудипоток
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(Objects.requireNonNull(Money.class.getResourceAsStream("cash_register.wav")));
             Clip clip = AudioSystem.getClip();
             clip.open(audioInputStream);
-            clip.start();
+            clip.start();//стартуем аудипоток
             while (!clip.isRunning()) {   //ждет пока не начнет проигрывание
-                Thread.sleep(10);
+                Thread.sleep(10); // ждать 10 миллисекунд
             }
             while (clip.isRunning()) {    //ждет пока музыка закончится
-                Thread.sleep(10);
+                Thread.sleep(10); // ждать 10 миллисекунд
             }
             clip.close();             //закрыть аудиопоток
             audioInputStream.close();
-        } catch (Exception e) {
+        } catch (Exception e) { // ловим исключение
             e.printStackTrace();
         }
     }
