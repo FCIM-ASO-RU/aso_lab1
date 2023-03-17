d37270609f70ce248ad94ad58d9dcd5ca1b42392
Novembver212 <vf.november212@gmail.com>
2023-03-17 16:37:42 +0200

    change/Fiodorov

--- src/aso_lab1/Main.java
+++ src/aso_lab1/Main.java
@@ -1,13 +1,13 @@
 package aso_lab1;
 
+
 import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
-
     public static void main(String[] args) {
         final Timer timer = new Timer();
-        
-        
+        TimerTask taskFiodorov = new Fiodorov();
+        timer.schedule(taskFiodorov, 4000);
     }
-    
 }
